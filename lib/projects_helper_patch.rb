d4af77fb01d24fc90fd95ab47d99d71f618bbff7
johnjeffersoncm <john27dom@hotmail.com>
2023-05-15 17:27:09 -0300

    Fix translation missing error on project homepage
    Solution for translation errors on the main project screen (problem identified in v5.05):
    
    
    translation missing: en.label_overall_spent_time
    translation missing: en.label_overall_activity
    Projects
    
    Recommended next steps: evaluate whether it makes sense to have these buttons in this location in v5.0.5, as by default these buttons already exist as tabs (active modules) just above the projects page. In previous versions it made sense to have them, as these buttons did not exist on the main page of the projects.

--- lib/projects_helper_patch.rb
+++ lib/projects_helper_patch.rb
@@ -15,9 +15,9 @@ module ProjectsHelperPatch
       end
     end
     if User.current.allowed_to?(:view_time_entries, nil, :global => true)
-      links << link_to(l(:label_overall_spent_time), time_entries_path)
+      links << link_to(l(:label_spent_time), time_entries_path)
     end
-    links << link_to(l(:label_overall_activity), activity_path)
+    links << link_to(l(:label_activity), activity_path)
     links.join(" | ").html_safe
   end
 end
